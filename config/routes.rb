23b415e4bd71bda2abc901190690ff68572861fd
azizdevfull <aziz16110904@gmail.com>
2022-11-21 19:36:25 +0500

    remove music's scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :narutos
-  resources :musics
   delete 'logout', to: 'users#destroy'
   resources :users, only: [:create]
   post "/login", to: "users#login"
